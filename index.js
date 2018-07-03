89ed702bca58644124628c8c71beafc5125e4ba8
AOleksenko <37514647+AOleksenko@users.noreply.github.com>
2018-07-03 17:18:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const _ = require ('lodash'); //функция для рандома
 const request= require('request');
-const TOKEN = '522735094:AAEWCVUByfkzYVgR7VAqbUewJaWqLJLwRcg'
+const TOKEN = 'your token'
 const fs = require('fs'); //для подключения картинки
 const bot = new TelegramBot(TOKEN, {
     polling: true
